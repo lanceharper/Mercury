ca37f87839d348f298a186450d53b20b7a0cd27d
lance <lance.harper@gmail.com>
2011-10-10 20:42:59 -0500

    Add images

--- Mercury.Web/Controllers/HomeController.cs
+++ Mercury.Web/Controllers/HomeController.cs
@@ -12,5 +12,26 @@ namespace Mercury.Web.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public JsonResult GetData()
+        {
+            var jsonData = new
+            {
+                total = 1,//pageCount,
+                page = 1,
+                records = 100, // implement later 
+                rows = (from n in Enumerable.Range(0, 25)
+                        select new
+                        {
+                            id = n,
+                            cell = new[]
+                                      {
+                                          "","First", "Last", "Phone", "Kansas City", "KS", "66109"
+                                      }
+                        }).ToArray()
+            };
+            return Json(jsonData);
+        }
     }
 }
